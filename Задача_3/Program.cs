0db1c55d1effc4339a896f1a5b462c4b8a5ff867
Aleksandr <tregubovao@mail.ru>
2022-10-27 15:21:17 +0300

    Задача_3_доработана

---
+++ Задача_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Написать программу масштабирования фигуры
+
+int xa = 0; int ya = 0;
+int xb = 0; int yb = 4;
+int xc = 8; int yc = 4;
+int xd = 8; int yd = 0;
+
+Console.WriteLine();
+Console.WriteLine($"Имеется прямоугольник АВСD с вершинами в координатах по оси x и оси y: ({xa};{ya}), ({xb};{yb}), ({xc};{yc}), ({xd};{yd}).");
+Console.WriteLine();
+Console.Write("Введите коэффициент масштабирования фигуры:  ");
+float k = float.Parse(Console.ReadLine() ?? "0");
+
+Console.WriteLine($"Вершины нового  прямоугольника АВСD с учетом коэффициента масштабирования {k}: ({xa * k};{ya * k}), ({xb * k};{yb * k}), ({xc * k};{yc * k}), ({xd * k};{yd * k}).");
+
